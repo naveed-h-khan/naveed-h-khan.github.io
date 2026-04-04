649f3c4857945347f57b2b9f3c20aad3f788d5bb
Naveed Khan <naveed@logicsoft.co.in>
2026-04-04 12:32:49 +0100

    Redesign pages

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,9 +1,11 @@
 $(function () {
   $("#site-header").load("/partials/header.html", function () {
     const path = window.location.pathname.toLowerCase();
-    const navKey = path.endsWith("/blog.html") ? "blog" : "home";
+    const navKey = path.endsWith("/blog.html") ? "blog" : "";
     $(`[data-nav="${navKey}"]`).addClass("is-active");
   });
 
-  $("#site-year").text(new Date().getFullYear());
+  $("#site-footer").load("/partials/footer.html", function () {
+    $("#site-year").text(new Date().getFullYear());
+  });
 });
